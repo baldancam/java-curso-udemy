926382a12f99dbc11ed4b3018d52d65e764444de
baldancam <baldancama@gmail.com>
2024-08-14 19:15:45 -0300

    tipos primitivos

---
+++ exercicios/src/fundamentos/Inferencia.java
@@ -0,0 +1,17 @@
+package fundamentos;
+
+public class Inferencia {
+
+	public static void main(String[] args) {
+		
+		double a = 4.5;
+		System.out.println(a);
+		
+		var b = 10.5;
+		System.out.println(b);
+		
+		var c = "texto";
+		System.out.println(c);
+	}
+	
+}
