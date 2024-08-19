a16afb04ab65f8442f9188952f6eac49cf7cac51
baldancam <baldancama@gmail.com>
2024-08-19 20:32:43 -0300

    primeiro desafio

---
+++ exercicios/src/fundamentos/PrimitivoVsObjeto.java
@@ -0,0 +1,16 @@
+package fundamentos;
+
+public class PrimitivoVsObjeto {
+	
+	public static void main(String[] args) {
+		
+		String s = new String ("texto");
+		s.toUpperCase();
+		
+		// Wrappers - São a ver~sao objeto dos tipor primitivos!
+		int a = 123;
+		System.out.println(a);
+		
+	}
+
+}
