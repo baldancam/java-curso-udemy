1448d6f0e1ce870aca2a584989bc4e9c81b4903a
baldancam <baldancama@gmail.com>
2024-12-09 20:53:43 -0300

    if/else

---
+++ exercicios/src/controle/IfDesafio.java
@@ -0,0 +1,20 @@
+package controle;
+
+public class IfDesafio {
+
+	public static void main(String[] args) {
+
+		double nota = 8.9;
+
+		if (nota >= 9.0) {
+
+			System.out.println("Quadro de Honra!");
+			System.out.println("Você é fera");
+
+		} else {
+			System.out.println("Deu Ruim!");
+		}
+
+	}
+
+}
