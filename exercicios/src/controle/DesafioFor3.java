12edf422b30dbfd690b02dd060c4b94eecc376ba
baldancam <baldancama@gmail.com>
2024-12-19 21:10:46 -0300

    desafio for

---
+++ exercicios/src/controle/DesafioFor3.java
@@ -0,0 +1,14 @@
+package controle;
+
+public class DesafioFor3 {
+
+	public static void main(String[] args) {
+
+		for (String i = "#"; !i.equals("######"); i += "#") {
+			System.out.println(i);
+
+		}
+
+	}
+
+}
---
+++ exercicios/src/controle/For2Desafio.java
@@ -0,0 +1,15 @@
+package controle;
+
+public class For2Desafio {
+
+	public static void main(String[] args) {
+
+		for (int i = 10; i >= 0; i -= 2) {
+			System.out.printf("Contador = %d\n", i);
+
+		}
+		;
+
+	}
+
+}
