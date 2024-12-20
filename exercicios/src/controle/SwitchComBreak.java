9e9a5d24c22983f5ae3bd11270ce2d6ff1083229
baldancam <baldancama@gmail.com>
2024-12-20 20:47:44 -0300

    break e continue

---
+++ exercicios/src/controle/SwitchComBreak.java
@@ -0,0 +1,46 @@
+package controle;
+
+import java.util.Scanner;
+
+public class SwitchComBreak {
+
+	public static void main(String[] args) {
+
+		Scanner entrada = new Scanner(System.in);
+
+		String conceito = "";
+
+		System.out.println("Informe a nota: ");
+		int nota = entrada.nextInt();
+
+		switch (nota) {
+		case 10:
+			conceito = "A";
+			break;
+		case 9:
+			conceito = "A-";
+			break;
+		case 8:
+			conceito = "B+";
+			break;
+		case 7:
+			conceito = "B";
+			break;
+		case 6:
+			conceito = "B-";
+			break;
+		case 5:
+			conceito = "C+";
+			break;
+		default:
+			conceito = "Conceito Não encontrado!";
+			break;
+		}
+
+		System.out.println("Conceito: " + conceito);
+
+		entrada.close();
+
+	}
+
+}
